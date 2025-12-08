ef8dba725897a16095ac2afd33d61a0c606a350b
pheldu95 <phelps.duncan@gmail.com>
2025-12-07 21:47:01 -0600

    added font awesome. SearchBar component using font awesome icons

---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+import { faSearch, faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function SearchBar() {
+    return (
+        <div className="relative">
+            <input
+                className="appearance-none border-2 pl-10 border-gray-300 
+                hover:border-gray-400 transition-colors rounded-md w-full 
+                py-2 px-3 text-gray-800 leading-tight focus:outline-none 
+                focus:ring-purple-600 focus:border-purple-600 focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Search..."
+            />
+            <button >
+                <div className="absolute right-0 inset-y-0 flex items-center">
+                    <FontAwesomeIcon icon={faX} className="mx-2 cursor-pointer" style={{color: 'grey'}} />
+                </div>
+            </button>
+
+            <button>
+                <div className="absolute left-0 inset-y-0 flex items-center">
+                    <FontAwesomeIcon icon={faSearch} className="mx-2 cursor-pointer" style={{color: 'grey'}} />
+                </div>
+            </button>
+        </div>
+    )
+}
--- client/src/features/Navbar.tsx
+++ client/src/features/Navbar.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
 
 export default function Navbar() {
   return (
     <div className="flex space-around justify-center h-10">
-      <div className="flex-1 items-center space-x-2">
-        My Store Page
+      <div className="flex-1 items-center space-x-2 flex">
+        <h1>My Store Page</h1>
+        <SearchBar />
       </div>
       <nav className='flex-1 sm:flex space-x-10 justify-end'>
         <NavLink to='/' className="mx-2">
